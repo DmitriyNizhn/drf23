60e2c7b1e064c8c4bd09358ef08e3879409bb6ac
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2023-03-02 00:20:14 +0300

    l2 - done

--- todomax/frontend/src/App.js
+++ todomax/frontend/src/App.js
@@ -1,25 +1,64 @@
+import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/User.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+//        const authors = [
+//            {
+//                'first_name': 'Фёдор',
+//                'last_name': 'Достоевский',
+//                'birthday_year': 1821
+//            },
+//            {
+//                'first_name': 'Александр',
+//                'last_name': 'Грин',
+//                'birthday_year': 1880
+//            },
+//        ]
+        axios.get(('http://127.0.0.1:8000/api/users')).then(response => {
+            this.setState(
+                {
+                    'users': response.data
+                }
+            )
+        }).catch(error => console.log(error))
+    }
+
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                </header>
+                <hr/>
+                <UserList users={this.state.users}/>
+
+                <hr/>
+                <footer>
+                    <cl>Fo</cl>
+                    <cl>te</cl>
+                    <cl>r</cl>
+                </footer>
+
+            </div>
+        )
+    }
 }
 
 export default App;
