9c516c9e5e1ad4c5e2d698c1a4f190c007dd071c
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2023-03-08 19:42:35 +0300

    lesson_4 user viewset update

--- todomax/todomax/settings.py
+++ todomax/todomax/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'users',
     'corsheaders',
     'TODO',
@@ -128,3 +129,14 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
--- todomax/users/views.py
+++ todomax/users/views.py
@@ -1,4 +1,6 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from .serializers import UserSerializer
 from .models import User
 
@@ -6,3 +8,12 @@ from .models import User
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+# class UserCustomViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+#                         mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class UserCustomViewSet(ListModelMixin,
+                        RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
